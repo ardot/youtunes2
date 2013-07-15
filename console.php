0f661fb928f8bc1b99689d2f2ccd65d7bed5fa19
Tim Lenardo <lenardot@lenardot-mbp.local>
2013-07-14 22:34:53 -0700

    Fixed signup issues

--- console.php
+++ console.php
@@ -4,121 +4,121 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<link rel="stylesheet" type="text/css" href="youTunesStyle.css" title="YouTunes Style"/>
-		<link rel="stylesheet" type="text/css" href="js/apprise/apprise.min.css" title="Apprise Style"/>
-		<script src="js2/jquery.js" type="text/javascript"></script>
-		<script src="js/googlejsapi.js" type="text/javascript"></script>
-		<script type="text/javascript" src="js/ytsearch.js"></script>
-		<script type="text/javascript" src="js/apprise/apprise-1.5.min.js"></script>
-		
-		</head>
-		<script type="text/javascript">
-   			var xmlHttp = null;
+  <head>
+    <link rel="stylesheet" type="text/css" href="youTunesStyle.css" title="YouTunes Style"/>
+    <link rel="stylesheet" type="text/css" href="js/apprise/apprise.min.css" title="Apprise Style"/>
+    <script src="js2/jquery.js" type="text/javascript"></script>
+    <script src="js/googlejsapi.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/ytsearch.js"></script>
+    <script type="text/javascript" src="js/apprise/apprise-1.5.min.js"></script>
 
-			function GetCustomerInfo()
-			{	
-    			var CustomerNumber = document.getElementById( "TextBoxCustomerNumber" ).value;
-    			var Url = "GetCustomerInfoAsJson.aspx?number=" + CustomerNumber;
+    </head>
+    <script type="text/javascript">
+      var xmlHttp = null;
 
-  			  	xmlHttp = new XMLHttpRequest(); 
-    			xmlHttp.onreadystatechange = ProcessRequest;
- 				xmlHttp.open( "GET", Url, true );
-    			xmlHttp.send( null );
-			}
+      function GetCustomerInfo()
+	{
+	var CustomerNumber = document.getElementById( "TextBoxCustomerNumber" ).value;
+	var Url = "GetCustomerInfoAsJson.aspx?number=" + CustomerNumber;
 
-			function ProcessRequest() 
-			{
-    			if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) 
-    			{
-        			if ( xmlHttp.responseText == "Not found" ) 
-        			{
-           			 	document.getElementById( "TextBoxCustomerName"    ).value = "Not found";
-            			document.getElementById( "TextBoxCustomerAddress" ).value = "";
-        			}
-        			else
-        			{
-            			var info = eval ( "(" + xmlHttp.responseText + ")" );
+		xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = ProcessRequest;
+		xmlHttp.open( "GET", Url, true );
+	xmlHttp.send( null );
+	}
 
-            			// No parsing necessary with JSON!        
-        				document.getElementById( "TextBoxCustomerName"    ).value = info.jsonData[ 0 ].cmname;
-            			document.getElementById( "TextBoxCustomerAddress" ).value = info.jsonData[ 0 ].cmaddr1;
-        			}                    
-    			}
-			}
-   
+	function ProcessRequest()
+	{
+	if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 )
+	{
+		if ( xmlHttp.responseText == "Not found" )
+		{
+			document.getElementById( "TextBoxCustomerName"    ).value = "Not found";
+		document.getElementById( "TextBoxCustomerAddress" ).value = "";
+		}
+		else
+		{
+		var info = eval ( "(" + xmlHttp.responseText + ")" );
+
+		// No parsing necessary with JSON!
+			document.getElementById( "TextBoxCustomerName"    ).value = info.jsonData[ 0 ].cmname;
+		document.getElementById( "TextBoxCustomerAddress" ).value = info.jsonData[ 0 ].cmaddr1;
+		}
+	}
+	}
+
    		</script>
-   		
+
 		<script type="text/javascript">
     	  google.load("swfobject", "2.1");
-   		 </script>  
-  
+   		 </script>
+
     	<!--script type="text/javascript" charset="utf-8">
 			var curSelected
-			
+
 			$(document).ready( function () {
   				$('tr').click( function () {
-  					
+
   					console.log("Here!");
   				});
 			} );
 		</script0-->
     	<script type="text/javascript">
-    	   
+
       var paused=false;
       var selected="none";
       /*
        * Change out the video that is playing
        */
-      
+
       // Update a particular HTML element with a new value
       function updateHTML(elmId, value) {
         document.getElementById(elmId).innerHTML = value;
       }
-      
-      
+
+
       // Loads the selected video into the player.
       function loadVideo() {
         var selectBox = document.getElementById("videoSelection");
         var videoID = selectBox.options[selectBox.selectedIndex].value
-        
+
         if(ytplayer) {
           ytplayer.loadVideoById(videoID);
         }
       }
-      
+
       function pause(sender){
-      	
+
       	if(ytplayer){
-      		
+
       		var state = ytplayer.getPlayerState();
-      		
+
       		if (state == 1){
       			ytplayer.pauseVideo();
       		}
       		else{
       			ytplayer.playVideo();
       		}
-    
+
       	}
       }
-      
 
-	
-      
-     
+
+
+
+
       // This function is called when an error is thrown by the player
       function onPlayerError(errorCode) {
         alert("An error occured of type:" + errorCode);
       }
-      
+
       // This function is automatically called by the player once it loads
       function onYouTubePlayerReady(playerId) {
         ytplayer = document.getElementById("ytPlayer");
         ytplayer.addEventListener("onError", "onPlayerError");
         initializePlayer();
       }
-      
+
       // The "main method" of this sample. Called when someone clicks "Run".
       function loadPlayer() {
         // The video to load
@@ -127,100 +127,100 @@
         var params = { allowScriptAccess: "always" };
         // The element id of the Flash embed
         var atts = { id: "ytPlayer" };
-        
+
         console.log("starting the swf object init");
         // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
-        swfobject.embedSWF("http://www.youtube.com/v/" + videoID + 
-                           "?version=3&enablejsapi=1&playerapiid=player1", 
+        swfobject.embedSWF("http://www.youtube.com/v/" + videoID +
+                           "?version=3&enablejsapi=1&playerapiid=player1",
                            "videoDiv", "100%", "100%", "9", null, null, params, atts);
         console.log("SWF object created!");
       }
-      
+
     /*  function init(){
       	var nameCategory=document.getElementById("name");
       	nameCategory.setAttribute
       }*/
-     
-      
+
+
       function _run() {
       	console.log("running!");
         loadPlayer();
         /*init();*/
       }
-      
+
       console.log("made it here!");
       google.setOnLoadCallback(_run);
     </script>
-	
+
 
 	<body>
 		<div id="popup">
-			
+
 		</div>
-		
+
 		<div id="sidepage" style="position:absolute; left:100%; top:0%; width:100%; height:100%; z-index:20; background-color: #ADF8BB">
 			<div id="videoDiv"></div>
-			
+
 		</div>
-		
+
 		<?php
 			include 'header.php';
 		?>
 		<?php
 			include 'leftColumn.php';
 		?>
-		
+
 		<div id="cont">
-			
+
 			<!--div id="rightColumn"></div-->
 			<div id="library">
-				
+
 				<div id="libraryContainer">
-					
+
 					<div id="playlistScroll" style="margin:0px; padding:0px; height:100%;width:100%;overflow:auto;\">
-					
+
 					<?php
 						include 'table.php';
 					?>
-						
+
 					</div>
 
 				</div>
-					
-					
+
+
 					<?php
-						
+
 						$db = mysql_connect("localhost","root", "root");
-						
+
 						//check to see if the database was connected to successfully
     					if (!$db){
        						echo "Could not connect to database" . mysql_error();
         					exit();
     					}//end if
-						
+
 						//try to connect to the specific database, kill the thread if not
 						$db_name = "youtunes";
    		 				if (!mysql_select_db($db_name, $db)){
         					die ("Could not select database") . mysql_error();
     					}//end if
-	
+
 						if($uID == 1){
 							$query = "SELECT * FROM Songs";
 						}
 						else{
     						$query= "SELECT * FROM Songs INNER JOIN ((SELECT song FROM HasSong WHERE user=" .mysql_real_escape_string($uID). ") AS T) ON Songs.sID=T.song";
 						}
-						
+
 						$sql=mysql_query($query);
     					//$sql=mysql_query("select * from Songs");
-    					
+
 						//counts the number of songs and stores them for communication with Javascript later
     					$index = 0;
 						$songs= array();
-						
-						
+
+
     					while($row=mysql_fetch_assoc($sql)){
-       						
+
 							$vID = $row['vID'];
 							$title = $row['title'];
 							$time = $row['time'];
@@ -229,17 +229,17 @@
 							$genre = $row['genre'];
 							$sID = $row['sID'];
 							$album = $row['album'];
-							
-							
+
+
 							$songs[$index] = array('vID' => $vID, 'title' => $title, 'time' => $time, 'artist' => $artist, 'plays' => $plays, 'genre' => $genre, 'sID' => $sID, 'album' => $album);
-							
+
 							//increment the counter so that the next video gets the next index
 							$index++;
-						}//end while	
-					
-					
+						}//end while
+
+
 					?>
-				
+
 				<script type="text/javascript" src="js/editSongs.js"></script>
 				<script type="text/javascript">
 					var selected=-1;
@@ -252,7 +252,7 @@
 					var titleArray = [];
 					var artistArray = [];
 					var songStack = [];
-   					var initialized = false; 
+   					var initialized = false;
    					var selected="nameSel";
    					var shuffle=false;
    					var loop=0;
@@ -264,18 +264,18 @@
    					var playingImg = document.createElement("img");
    					playingImg.setAttribute("src", "images/sound_high.png");
    					playingImg.setAttribute("alt", ">");
-   				
-   					
-   					
+
+
+
    					/**
    					 * Runs through all of the songs, and cues them into the player as a playlist
    					 */
    					function initializePlayer(){
    						<?php echo("var numSongs = $index;"); ?>
-					
+
 						//Prints the songs into javascript from PHP
 						var num = parseInt(numSongs);
-				 				<?php 
+				 				<?php
 				 					//prints out the songID, title and author to be stored in Javascript
 									print ("var songs = \"");
 									foreach ($songs as $vid){
@@ -289,37 +289,37 @@
 								?>
 						//console.log(songs);
 						var infoArray = songs.split(";");
-						
-						
+
+
 						//run through the info and store them in songArray, titleArray, and artistArray accordingly
 						for(var i = 0; i < infoArray.length; i++){
-							
+
 							var spltArr = infoArray[i].split("<>");
 							var id = spltArr[3];
 							index_map[id] = i;
-							
+
 							songArray[i] = spltArr[0];
 							titleArray[i] = spltArr[1];
 							artistArray[i] = spltArr[2];
 						}//end for
 						console.log(songArray);
 						if(ytplayer){
-   							//set an event listener, and cue the playlist. 
+   							//set an event listener, and cue the playlist.
    							ytplayer.addEventListener("onStateChange", "onPlayerStateChange");
    							ytplayer.addEventListener('onError', 'onPlayerError');
    							//ytplayer.cuePlaylist(songArray,0,0,"small");
    						}//end if
    						setInterval(updatePlayerInfo, 250);
    					}//end initialize player
-   					
+
    					function onPlayerError(){
    						console.log('Error');
    					}
-   					//Changes the class of a header when the user clicks on it 
+   					//Changes the class of a header when the user clicks on it
       				function selectHeader(sender){
       					console.log("here");
       					var id = sender.getAttribute("id");
-      					
+
       					//checks if that header is already selected. If not change it
       					if(selected != id){
       						var previous = document.getElementById(selected);
@@ -329,31 +329,31 @@
       						selected = id + "Sel";
       						sender.setAttribute("id", id + "Sel");
       					}//end if
-      				
+
       				}//end selectHeader
-   					
-   				
-   					
+
+
+
    					function updatePlayerInfo(){
-   						
+
    						if(ytplayer && ytplayer.getDuration){
-   							
+
    							var length = ytplayer.getDuration();
    							var curTime = ytplayer.getCurrentTime();
    							var total = ytplayer.getVideoBytesTotal();
    							var loaded = ytplayer.getVideoBytesLoaded();
-   							
+
    							var percent = curTime/length * 100;
    							var loadedPercent = loaded/total * 100;
-   							
+
    							var stringPercent = percent.toString();
    							var stringLoaded = loadedPercent.toString();
    							progress.style.width = stringPercent.concat("%");
    							loadedBar.style.width = stringLoaded.concat("%");
    							/*console.log("UpdatingPlayerInfo");*/
-   						}	
+   						}
    					}
-   					
+
    					/**
    					 * SetShuffle
    					 * Resets the shuffle value depending on its current value
@@ -369,7 +369,7 @@
    							console.log(shuffle);
    						}//end else
    					}//end setShuffle
-   					
+
    					/**
    					 * setLoop
    					 * Sets the loop value to either no loop, loop, or single song loop
@@ -394,22 +394,22 @@
    							loop=0;
    						}//end else
    					}//end setLoop
-   				
+
    					function chooseNextSong(next){
    						//loop!
    								if(loop==1){
    									console.log("here!");
    									var row = $(playing).next();
-   									
+
    									if(next == 0){
    										row =$(playing).prev();
    									}
-   									
-   									
+
+
    									console.log(row);
    									if(row.length == 0){
    										console.log("here!");
-   										
+
    										play(playing.parentNode.childNodes[2]);
    									}
    									else{
@@ -418,12 +418,12 @@
    								}
    								//if shuffle is set, play a random song
    								else if(shuffle == true){
-   									
+
    									var nodes = playing.parentNode.childNodes;
-   									
+
    									//ytplayer.stopVideo();
    									var nextSong=Math.floor(Math.random() * Object.keys(index_map).length);
-   									
+
    									play(nodes[nextSong + 2]);
    									//console.log("Next song:" + nextSong + "\n\n");
    									//PlayIndex(nextSong);
@@ -431,43 +431,43 @@
    								//TODO otherwise somehow determine the next song
    								else{
    									var row = $(playing).next();
-   									
+
    									if(next == 0){
    										row =$(playing).prev();
    									}
-   								
+
    									if(row.length > 0){
-   										play(row[0]);	
-   									}	
+   										play(row[0]);
+   									}
    								}
    					}
-   				
+
    					function back(){
    						var curTime = ytplayer.getCurrentTime();
-      					
+
       					if(curTime > 1){
       						play(songStack.pop());
       					}
       					else{
       						songStack.pop();
       						if(songStack.length > 0){
-      							play(songStack.pop());	
+      							play(songStack.pop());
       						}
-      						
+
       					}
       					//chooseNextSong(0);
       				}
-      
+
       				function forward(){
       					chooseNextSong(1);
       				}
-      
+
    					//Handles the state changes accordingly
    					function onPlayerStateChange(newState){
    						//TODO When a song has ended, increment its play count, find the next song, and play that one
-   						
+
 						console.log(newState);
-   						
+
    						if(newState == 0){
    							//if(justChose == false){
    								//console.log("Song ended " + shuffle + "  " + justChose);
@@ -487,21 +487,21 @@
    						else if(newState == 2){
    							playButton.src = "images/play.png";
    						}
-   					
+
    					}
-   					
+
    					function play(sender){
 						console.log("Play!");
-			
+
 						if(ytplayer){
-						
+
 							var songID = sender.getAttribute("id");
 							var vID = sender.getAttribute("name");
 							var songIndex = parseInt(songID);
 							//ytplayer.playVideoAt(songIndex);
-							
+
 							songStack.push(sender);
-							
+
 							playing = sender;
 							var play = sender.childNodes[0].childNodes[0];
 							if(play == undefined){
@@ -510,56 +510,56 @@
 							else{
 								play.appendChild(playingImg);
 							}
-							
-							
+
+
 							titlePane.innerHTML = titleArray[songIndex];
    							artistPane.innerHTML = artistArray[songIndex];
-   							
+
    							ytplayer.loadVideoById(vID);
 						}
 					}
-					
+
 					function deleteSong(sender){
-						
+
 						console.log(sender.parentNode.parentNode);
-						
+
 						var r = confirm("Are you sure you want to delete this song?");
-						
+
 						if (r==true){
  							var to_remove = sender.parentNode.parentNode.parentNode;
  							var sID = to_remove.getAttribute( "title");
- 							
+
  							console.log(to_remove);
  							var url = "delete.php";
 							url = url.concat("?sID=" + sID);
 							console.log(url);
 							$.get(url);
- 							
+
  							to_remove.parentNode.removeChild(to_remove);
- 							$("table").trigger("update"); 
+ 							$("table").trigger("update");
   						}
-						
+
 					}
-		
+
 					function addPlaylist(){
-						
-						
+
+
 					}
-					
+
    					//Plays the song selected (called on a double click)
    					/*function Play(sender)
    					{
-   						
+
    						var index = sender.getAttribute("id");
    						var splitIndex = index.split("_");
    						var vIndex = parseInt(splitIndex[1]);
-   						
+
    						justChose = true;
    						currentSongPlaying = vIndex;
    						PlayIndex(vIndex);
-   						
+
    					}//end play
-   					
+
    					function PlayIndex(index){
    						setInterval(updatePlayerInfo, 250);
    						if(ytplayer){
@@ -572,21 +572,21 @@
    							play.src = "images/pause.png";
    						}//end if
    					}//end playIndex*/
-   					
+
    				</script>
-			
-						
+
+
 			</div>
-			
-		
-		
-			
+
+
+
+
 		</div>
-		
+
 		<?php
 			include 'footer.php';
 		?>
-		
+
 	</body>
 </html>
 
--- signup.php
+++ signup.php
@@ -1,100 +1,105 @@
 <?php
-
 	session_start();
-	
-	//print("<h> Connectiong </h>");
-	
+
 	function encrypt_password($text) {
 		$salty = $test . "Friends to this ground. And leigemen to the Dane!";
 		return hash("sha256", $salty);
 	}
-	
+
 	print("<p> Here </p>");
-	
-	//check to see if the user has attempted to log in	
-	if (isset($_POST['username_new']) && isset($_POST['passwd_new']) && isset($_POST['passwdconf_new'])) {
+
+	// check to see if the user has attempted to log in
+	if (isset($_POST['username_new']) &&
+      isset($_POST['passwd_new']) &&
+      isset($_POST['passwdconf_new'])) {
+
 		print("<p> Here </p>");
-		
-		$db = mysql_connect("localhost","root", "root");
-						
-		//check to see if the database was connected to successfully
-    	if (!$db) {
-       		echo "Could not connect to database" . mysql_error();
-        	exit();
-    	}//end if
-						
-		//try to connect to the specific database, kill the thread if not
+
+		$db = mysql_connect("localhost", "root", "root");
+
+		// check to see if the database was connected to successfully
+    if (!$db) {
+      echo "Could not connect to database" . mysql_error();
+      exit();
+    } //end if
+
+		// try to connect to the specific database, kill the thread if not
 		$db_name = "youtunes";
-   		if (!mysql_select_db($db_name, $db)) {
-        	die ("Could not select database") . mysql_error();
-    	}//end if
-		
-		
+   	if (!mysql_select_db($db_name, $db)) {
+      die ("Could not select database") . mysql_error();
+    } //end if
+
+
 		$username = $_POST['username_new'];
 		$passwd = $_POST['passwd_new'];
 		$passwd_conf = $_POST['passwdconf_new'];
-		
+
 		if (strcmp($passwd, $passwd_conf) != 0) {
-			$_SESSION['login_error'] = "Password and Confirm Password do not match!";	
+			$_SESSION['login_error'] = "Password and Confirm Password do not match!";
 		}
-		else { 
+		else {
 			$query = "SELECT * FROM Users WHERE username='" .mysql_real_escape_string($username). "'";
-			
+
 			print("<p> $query </p>");
-			
+
 			$result = mysql_query($query);
-		
+
 			$continue = false;
-			print("<p> $continue </p>");		
+			print("<p> $continue </p>");
 			while ($row=mysql_fetch_assoc($result)) {
 				print("<p> $row </p>");
 				$continue = true;
 			}
-					
+
 			print("<p> $continue </p>");
-			
+
 			if ($continue) {
 				$_SESSION['login_error'] = "A user with that email already exists!";
 			}
 			else {
-				$query = "INSERT INTO  `youtunes`.`Users` (
-						`uID`,
-						`username`,
-						`password`,
-					)
-					VALUES (
-						NULL,
-						'" .mysql_real_escape_string($username). ",
-						'" .mysql_real_escape_string($passwd). "',
-					);";
-				$result = mysql_query($query);
-		
-				
-				$query = "SELECT LAST_INSERT_ID();";
-				$result = mysql_query($query);
+
+	      print("<p> Here </p>");
+				$insert = sprintf("INSERT INTO  Users
+            (username, password)
+					VALUES
+            (\"%s\", \"%s\")",
+          mysql_real_escape_string($username),
+					mysql_real_escape_string($passwd));
+
+        $result2 = mysql_query($insert);
+
+        $error = mysql_error();
+
+			  print("<p> $insert </p>");
+			  print("<p> $error </p>");
+			  print("<p> $result2 </p>");
+
+        $query3 = "SELECT LAST_INSERT_ID();";
+				$result3 = mysql_query($query3);
 				$found = false;
-				
-				while ($row=mysql_fetch_assoc($result)) {
+
+			  print("<p> $result3 </p>");
+
+        while ($row=mysql_fetch_assoc($result3)) {
 					$_SESSION['uid'] = $row['uID'];
 					$_SESSION['username'] = $username;
 					$found = true;
-				}	
-			
+				}
+
 				if (!$found) {
 					$_SESSION['login_error'] = "Error while adding user, please try again!";
 				}
-				
-				
-			}	
+
+
+			}
 		}
-		
-		 
+
+
 	} else {
 		$_SESSION['login_error'] = "Please fill out all of the required fields!";
 	}
-	
-	//header("Location: index.php");
 
-	
+	header("Location: index.php");
+
 ?>
-		
+
