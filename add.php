8b9e5d8280ba2beb871acf5168b003e969a3d377
Tim Lenardo <lenardot@lenardot-mbp.local>
2013-08-06 00:42:24 -0700

    Removed prints

--- add.php
+++ add.php
@@ -19,14 +19,10 @@
 
 	//try to connect to the specific database, kill the thread if not
 	$db_name = "youtunes";
-   	if (!mysql_select_db($db_name, $db)){
-    	die ("Could not select database") . mysql_error();
-    }//end if
+  if (!mysql_select_db($db_name, $db)){
+    die ("Could not select database") . mysql_error();
+  }
 
-	/*mysql_query("INSERT INTO  `youtunes`.`Users`
-					(`uID` ,`username` , `password`)
-				VALUES
-					(NULL ,  'Tim',  'Lenardo')");*/
 
 
 	if((!isset($_GET['album'])) || (!isset($_GET['time'])) || (!isset($_GET['vID'])) || (!isset($_GET['plays']))){
@@ -46,12 +42,9 @@
 	$title = $_GET['title'];
 	$time = $_GET['time'];
 	$artist = $_GET['artist'];
+  $uID = $_SESSION['uid'];
 
-	//print("<p> Album: $album <br/> </p>");
-
-  	$uID = $_SESSION['uid'];
-
-  	$query = "INSERT INTO  `youtunes`.`Songs` (`sID`, `title`, `time`, `artist`, `genre`, `plays`, `vID`, `album`) VALUES (NULL ,  '$title',  '$time',  '$artist',  '$genre',  '0',  '$vID',  '$album')";
+  $query = "INSERT INTO  `youtunes`.`Songs` (`sID`, `title`, `time`, `artist`, `genre`, `plays`, `vID`, `album`) VALUES (NULL ,  '$title',  '$time',  '$artist',  '$genre',  '0',  '$vID',  '$album')";
 	mysql_query($query);
 
 	$last_id_query = "SELECT LAST_INSERT_ID();";
@@ -62,32 +55,9 @@
 		$last_int_id = $row[0];
 	}
 
-	print("<h1>$last_int_id</h1>");
-
 	$has_song_insert = "INSERT INTO HasSong (song, user, plays) VALUES ($last_int_id, " .mysql_real_escape_string($uID). ", 0)";
   $result2 = mysql_query($has_song_insert);
-  print("<h1> $result2 </h1>");
 
-
-	/*while($row=mysql_fetch_assoc($result)){
-		$sID = $row['sID'];
-		print("<h1> sID = $sID </h1>");
-$uID = $_SESSION['uid'];
-		print("<h1> uID = $uID </h1>");
-				print("<h1> query = $query </h1>");
-		$result = mysql_query($query);
-		print("<h1> result = $result </h1>");
-	}*/
-	//print("<h> $result </h>");
-
-
-
- /*
-	mysql_query("INSERT INTO 'youtunes'.'Songs'
-					('sID', 'album', 'vID', 'plays', 'genre', 'title', 'time', 'artist')
-				VALUES
-					(NULL, 'album', NULL, NULL, NULL, NULL, NULL, NULL ");//'album', '$vID', '$plays', '$genre', '$title', '$time', '$artist')");
-	*/
 	mysql_close($db);
 
 	echo $last_int_id;
